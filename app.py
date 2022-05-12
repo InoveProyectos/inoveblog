c920733983399f39eaffa89f5df02f9eb0aa3251
inoveProyectos <info@inove.com.ar>
2022-05-12 20:22:45 -0300

    Implementar swagger

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import requests
 from datetime import datetime
 
 import traceback
-from flask import Flask, request, jsonify, render_template, Response, redirect, url_for
+from flask import Flask, request, make_response, jsonify, render_template, Response, redirect, url_for
 
 # Crear el server Flask
 app = Flask(__name__)
@@ -21,8 +21,18 @@ db.init_app(app)
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(1000))
-    apikey = db.Column(db.String(100))
+    name = db.Column(db.String(50))
+    apikey = db.Column(db.String(50))
+
+
+class Post(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(100))
+    texto = db.Column(db.String(300))
+
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    user = db.relationship("User")
 
 # Configurar el sistema de login
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
@@ -40,41 +50,11 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 LOGIN_PASSWORD = "inovejs"
 
-@app.route('/login', methods=['GET', 'POST'])
+# ------------ Views ----------------- #
+
+@app.route('/login')
 def login():
-    try:
-        if request.method == 'GET':
-            return render_template('login.html')
-        if request.method == 'POST':
-            usuario = request.form['usuario']
-            password = request.form['password']
-            if password != LOGIN_PASSWORD:
-                print("LOGIN_PASSWORD incorrect")
-                return Response(status=400)
-
-            url = "http://23.92.69.190/administracion/usuarios/exists/"
-            payload = {"username": usuario}
-            res = requests.post(url, data=json.dumps(payload), headers={"Content-Type":"application/json"})
-            if res.status_code != 200:
-                print("USER incorrect")
-                return Response(status=400)
-
-            user = User.query.filter_by(name=usuario).first()
-            # Si el usuario existe no puedo crearlo
-            if user is None:
-                user = User(name=usuario, apikey="ABC123")
-                # add the new user to the database
-                db.session.add(user)
-                db.session.commit()
-
-            login_user(user)
-            return Response(status=200)
-
-    except Exception as e:
-        print(e)
-        print(jsonify({'trace': traceback.format_exc()}))
-        return Response(status=400)
-
+    return render_template('login.html')
 
 @app.route('/logout')
 @login_required
@@ -107,21 +87,105 @@ def foto():
         return Response(status=400)
 
 
-@app.route('/inoveblog/api/post', methods=['GET', 'POST'])
+# Referencia:
+# https://swagger.io/specification/
+@app.route('/docs')
+def docs():
+    return render_template('swaggerui.html')
+
+# ---------------- API ------------------------
+
+@app.route('/api/v1.0/login', methods=['POST'])
+def api_login():
+    try:
+        # Validate request data format
+        content_type = request.headers.get('Content-Type')
+        if (content_type != 'application/x-www-form-urlencoded'):
+            return Response(status=415)
+
+        # Read data
+        usuario = request.form['usuario']
+        password = request.form['password']
+
+        # Process request
+        if password != LOGIN_PASSWORD:
+            print("LOGIN_PASSWORD incorrect")
+            return Response(status=401)
+
+        url = "http://23.92.69.190/administracion/usuarios/exists/"
+        payload = {"username": usuario}
+        res = requests.post(url, data=json.dumps(payload), headers={"Content-Type":"application/json"})
+        if res.status_code != 200:
+            print("USER incorrect")
+            return Response(status=401)
+
+        user = User.query.filter_by(name=usuario).first()
+        # Si el usuario existe no puedo crearlo
+        if user is None:
+            user = User(name=usuario, apikey="ABC123")
+            # agregar usuario a la base de datos
+            db.session.add(user)
+            db.session.commit()
+
+        login_user(user)
+        return make_response(jsonify({"apikey": user.apikey}), 200)
+
+    except Exception as e:
+        print(e)
+        print(jsonify({'trace': traceback.format_exc()}))
+        return Response(status=401)
+
+
+@app.route('/api/v1.0/post', methods=['GET', 'POST'])
 def post():
     try:
         if request.method == 'GET':
-            pass
+            usuario = request.args.get('usuario')
+            apikey = request.args.get('apikey')
+
+            if usuario is None or apikey is None:
+                return Response(status=400)
+
+            user = User.query.filter_by(name=usuario).first()
+            if user is None or user.apikey != apikey:
+                # Si el usuario no existe o la apikey es invalida salgo
+                return Response(status=400)
+    
+            posts = []
+            for post in Post.query.filter_by(user=user).order_by(Post.id.desc()).limit(3):
+                posts.append({"titulo": post.titulo, "texto": post.texto})
+            return make_response(jsonify({"posts": posts}), 200)
+
 
         if request.method == 'POST':
-            usuario = request.form['usuario']
-            titulo = request.form['titulo']
-            texto = request.form['texto']
+            # Validate request data format
+            content_type = request.headers.get('Content-Type')
+            if (content_type != 'application/json'):
+                return Response(status=415)
+
+            usuario = request.json['usuario']
+            apikey = request.json['apikey']
+            titulo = request.json['titulo']
+            texto = request.json['texto']
             print(usuario)
+            print(apikey)
             print(titulo)
             print(texto)
-            return redirect(url_for('blog', usuario=usuario))
-    except:
+
+            user = User.query.filter_by(name=usuario).first()
+            if user is None or user.apikey != apikey:
+                # Si el usuario no existe o la apikey es invalida salgo
+                return Response(status=400)
+
+            post = Post(user=user, titulo=titulo, texto=texto)
+            # agregar post a la base de datos
+            db.session.add(post)
+            db.session.commit()
+
+            return make_response(jsonify({"id": post.id, "titulo": post.titulo, "texto": post.texto}), 200)
+    except Exception as e:
+        print(e)
+        print(jsonify({'trace': traceback.format_exc()}))
         return Response(status=400)
 
 # Este método se ejecutará solo una vez
@@ -137,4 +201,4 @@ if __name__ == '__main__':
     print('Inove@Server start!')
 
     # Lanzar server
-    app.run(host="127.0.0.1", port=5000, use_reloader=True)
+    app.run(host="127.0.0.1", port=5000)
--- static/js/blog.js
+++ static/js/blog.js
@@ -4,7 +4,26 @@ document.querySelector("#subirFoto").onclick = async () => {
     let formData = new FormData();
     const fileField = document.querySelector("#foto");
     const usuario = document.querySelector("#usuario").value;
+    const apikey = document.querySelector("#apikey").value;
     const url = document.querySelector("#subirFoto").getAttribute("path");
+    
+    formData.append('usuario', usuario);
+    formData.append('apikey', apikey);
+    formData.append('foto', fileField.files[0]);
+    
+    fetch(url, {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => location.reload())
+    .catch(error => console.error('Error:', error))
+}
+
+document.querySelector("#publicar").onclick = async () => {
+    let formData = new FormData();
+    const usuario = document.querySelector("#usuario").value;
+    const apikey = document.querySelector("#apikey").value;
+    const url = document.querySelector("#publicar").getAttribute("path");
     
     formData.append('usuario', usuario);
     formData.append('foto', fileField.files[0]);
@@ -16,4 +35,37 @@ document.querySelector("#subirFoto").onclick = async () => {
     .then(response => location.reload())
     .catch(error => console.error('Error:', error))
 }
+
+
+const usuario = document.querySelector("#usuario").value;
+const apikey = document.querySelector("#apikey").value;
+const url = document.querySelector("#publicar").getAttribute("path");
+
+const params = { 
+    usuario: usuario,
+    apikey: apikey,
+};
+// this line takes the params object and builds the query string
+const query = Object.keys(params)
+             .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
+             .join('&');
+
+fetch(url + "?" + query)
+    .then(response => response.json())
+    .then(data => data.posts)
+    .then(posts => {
+        let accumulator = ""
+        
+        posts.forEach(post => {
+            accumulator += 
+                `
+                <div>
+                    <p>${post.titulo}</p>
+                    <p>${post.texto}</p>
+                </div>
+                `
+        });
+        const section = document.querySelector("#posteos");
+        section.innerHTML = accumulator;
+    })
 
